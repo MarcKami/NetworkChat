3f11effe27dd3406416266cc3f1b8a86c4b66cb8
maventurer <molivertauler@gmail.com>
2018-02-15 18:34:36 +0100

    hiii

--- Network01/Source/Protocol.h
+++ Network01/Source/Protocol.h
@@ -1,14 +1,23 @@
 #include <SFML\Network.hpp>
+#include <SFML\Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include "Utils.hpp"
+
+using namespace utils;
+using namespace std;
 
 class Protocol {
 public:
 	Protocol();
 	~Protocol();
 
-	virtual void Enter() = 0;
-	virtual void Update() = 0;
-	virtual void Exit() = 0;
-
+	virtual void Run();
 private:
 
+protected:
+	sf::TcpSocket* socket;
+	vector<pair<string, originText>>* aMensajes;
 };
---
+++ Network01/Source/Utils.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace utils {
+
+	enum originText { mine, other };
+
+	#define SCREEN_WIDTH 800
+	#define SCREEN_HEIGHT 600
+
+	#define PORT 5000
+	#define MAX_MENSAJES 30
+	#define MAX_LENGTH 100
+
+	#define MINE_COLOR sf::Color(0, 160, 0)
+	#define OTHER_COLOR sf::Color(0, 160, 160)
+}
