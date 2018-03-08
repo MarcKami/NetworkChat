9a6e3d1d04c0ff6838ec14a04acbfe69e505ffef
MarcKami <Marc.kami1991@gmail.com>
2018-03-08 11:08:47 +0100

    Funcionaaaa!!!!!

--- Client_P2P/Main.cpp
+++ Client_P2P/Main.cpp
@@ -5,40 +5,59 @@
 
 void main() {
 	Direction dir[MAX_PLAYERS - 1];
-	sf::TcpSocket sock[MAX_PLAYERS - 1];
+	vector<sf::TcpSocket*> sock;
+	sf::TcpSocket serverSock;
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	sf::TcpListener listener;
 
 #pragma region Connection
 	//Connect to server
-	if (sock[0].connect(ip, PORT) != sf::Socket::Done)
+	if (serverSock.connect(ip, PORT) != sf::Socket::Done)
 		cout << "No se puede conectar al servidor" << endl;
 	else {
 		sf::Packet peers;
 		cout << "Esperando Peers" << endl;
-		if(sock[0].receive(peers) != sf::Socket::Done)
+		if(serverSock.receive(peers) != sf::Socket::Done)
 			cout << "No se recibieron los otros peers" << endl;
 		else {
-			sock[0].disconnect();
+			unsigned short myPort = serverSock.getLocalPort();
+			serverSock.disconnect();
 			cout << "Peers recibidos" << endl;
-			int size;
-			peers >> size;
-			for (int i = 0; i < size; i++) {
+			int players;
+			peers >> players;
+			for (int i = 0; i < players; i++) {
+				sf::TcpSocket* auxSock = new sf::TcpSocket();
 				peers >> dir[i].ip >> dir[i].port;
 				cout << "Socket " << i << ": IP - " << dir[i].ip << " PORT - " << dir[i].port << endl;
-				if (sock[i].connect(dir[i].ip, dir[i].port) != sf::Socket::Done) {
-					cout << "Imposible conectar al peer " << i << endl;
+				if (auxSock->connect(ip, dir[i].port) != sf::Socket::Done) {
+					cout << "Imposible conectar al peer: " << i << endl;
+				}
+				else {
+					cout << "Conexion con exito al peer: " << i << endl;
+					sock.push_back(auxSock);
 				}
 			}
+			if (listener.listen(myPort) != sf::Socket::Done) {
+				cout << "No se puede vincular al puerto 5000" << endl;
+			}
+			for (int i = 0; i < MAX_PLAYERS - 1 - players; i++) {
+				sf::TcpSocket* auxSock = new sf::TcpSocket();
+				if (listener.accept(*auxSock) == sf::Socket::Done)
+					sock.push_back(auxSock);
+			}
+			listener.close();
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		cout << "Socket " << i << ": " << sock[i].getRemotePort() << endl;
+	for each (sf::TcpSocket* s in sock) {
+		cout << "Socket: IP - " << s->getRemoteAddress().toString() << " PORT - " << s->getRemotePort() << endl;
 	}
+
 	int xwfw;
-	cin >> xwfw;
+	std::cin >> xwfw;
 #pragma endregion
 
-
-
+	for each (sf::TcpSocket* s in sock) {
+		s->disconnect();
+	}
 }
--- Server_P2P/Main.cpp
+++ Server_P2P/Main.cpp
@@ -3,46 +3,54 @@
 #include "Direction.h"
 #include "Utils.h"
 
-void RecieveClients(sf::TcpListener* listener, vector<Direction>* aPeers, sf::TcpSocket* sock) {
-	for (int i = 0; i < MAX_PLAYERS; i++) {
-		listener->accept(sock[i]);
-		Direction d(sock[i].getRemoteAddress().toString(), sock[i].getRemotePort());
-		cout << "Socket " << i << ": IP - " << d.ip << " PORT - " << d.port << endl;
-		aPeers->push_back(d);
-	}
-}
-
-void ConnectClients(vector<Direction> aPeers, sf::TcpSocket* sock) {
-	for (int i = 0; i < MAX_PLAYERS; i++) {
-		sf::Packet pack;
-		int idx = 0;
-		pack << MAX_PLAYERS - 1;
-		for (int j = 0; j < MAX_PLAYERS; j++) {
-			if (j != i)
-				pack << aPeers[j].ip << aPeers[j].port;
-		}
-		sock[i].send(pack);
-	}
-}
+//void RecieveClients(sf::TcpListener* listener, vector<Direction>* aPeers, sf::TcpSocket* sock) {
+//	for (int i = 0; i < MAX_PLAYERS; i++) {
+//		listener->accept(sock[i]);
+//		Direction d(sock[i].getRemoteAddress().toString(), sock[i].getRemotePort());
+//		cout << "Socket " << i << ": IP - " << d.ip << " PORT - " << d.port << endl;
+//		aPeers->push_back(d);
+//	}
+//}
+//
+//void ConnectClients(vector<Direction> aPeers, sf::TcpSocket* sock) {
+//	for (int i = 0; i < MAX_PLAYERS; i++) {
+//		sf::Packet pack;
+//		int idx = 0;
+//		pack << MAX_PLAYERS - 1;
+//		for (int j = 0; j < MAX_PLAYERS; j++) {
+//			if (j != i)
+//				pack << aPeers[j].ip << aPeers[j].port;
+//		}
+//		sock[i].send(pack);
+//	}
+//}
 
 void main() {
 	//ESTABLECER CONEXION
 	vector<Direction> aPeers;
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 	sf::TcpListener listener;
-	sf::TcpSocket sock[MAX_PLAYERS];
 	
 	if (listener.listen(PORT) != sf::Socket::Done) {
-		cout << "No se puede vincular al puerto 5000\n";
+		cout << "No se puede vincular al puerto 5000" << endl;
 	}
 
 	//Connect clients
-	RecieveClients(&listener, &aPeers, sock);
-	ConnectClients(aPeers, sock);
+	for (int i = 0; i < MAX_PLAYERS; i++) {
+		sf::TcpSocket sock;
+		listener.accept(sock);
+		Direction d(sock.getRemoteAddress().toString(), sock.getRemotePort());
+		cout << "Socket " << i << ": IP - " << d.ip << " PORT - " << d.port << endl;
 
-	//Disconnect server
-	//listener.close();
-	//for (int i = 0; i < MAX_PLAYERS; i++)
-	//	sock[i].disconnect();
-	//delete[] sock;
+		sf::Packet pack;
+		pack << i;
+		for (int j = 0; j < i; j++) {
+			pack << aPeers[j].ip << aPeers[j].port;
+		}
+		sock.send(pack);
+		aPeers.push_back(d);
+
+		sock.disconnect();
+	}
+	listener.close();
 }
